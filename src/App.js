e75e8f798ca4e4cb672037fe25ad8e92b9b0cccc
sdinakarbabu <sdinakarbabu111@gmail.com>
2024-12-30 17:03:51 +0530

    create task box completed on 30-12-2024

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
 function App() {
+  const addTask = ()=>{
+    var searchInputEle = document.getElementById("searchInput").value;
+    var fetchdate = new Date();
+    var Data = {
+      taskName:searchInputEle,
+      time: fetchdate.toLocaleTimeString(),
+      date:fetchdate.toLocaleDateString()
+    }
+    console.log(Data)
+  }
   return (
     <main>
       <header className="headerContainer">
         <h1 className="appHeading">TodoList</h1>
       </header>
+      <section>
+        <input type="text" placeholder="Enter Task" id="searchInput" />
+        <button type="button" className="btn" onClick={addTask}>Create</button>
+      </section>
     </main>
   );
 }
